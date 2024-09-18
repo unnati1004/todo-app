1de0657b975f41a3e1236dea5ad94d8e4a5b0b25
Unnati Gandhi <95961486+unnati1004@users.noreply.github.com>
2024-09-18 12:17:01 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
-import { Todo } from './component/todo';
+import "./App.css";
+import { BrowserRouter as Router, Route,Routes, Switch } from "react-router-dom";
+import { Todo } from "./component/todo";
 
 function App() {
   return (
     <div className="App">
-     <Todo/>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Todo />} />
+          {/* Add more routes here */}
+        </Routes>
+      </Router>
+
     </div>
   );
 }

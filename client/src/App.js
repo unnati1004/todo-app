c30d9358c04536520b6cdfd36baaaa0803bf0718
Unnati Gandhi <95961486+unnati1004@users.noreply.github.com>
2024-09-18 11:43:27 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Todo } from './component/todo';
-import { TodoRef } from './component/todoRef';
 
 function App() {
   return (
     <div className="App">
      <Todo/>
-     {/* <TodoRef/> */}
     </div>
   );
 }
